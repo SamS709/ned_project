12ea293f7385919da7c7d795596a5589a3c64759
Sami LEROUX <sami.leroux75@gmail.com>
2026-02-11 12:52:13 +0100

    new bip avaible crossplatform

--- Connect4/test.py
+++ Connect4/test.py
@@ -1,21 +1,10 @@
-import keras
+import pygame
+import os
+import time
 
-model = keras.models.Sequential([
-    keras.layers.Dense(50, input_shape=[50,]),
-    keras.layers.Dense(50)
-])
+pygame.mixer.init()
+pygame.mixer.music.load('Connect4/beep.mp3')
+pygame.mixer.music.play()
 
-layers = model.layers
-cfg = model.get_config()
-print(layers)
-print(cfg)
-print(len(cfg['layers']))
-n_layers = len(cfg['layers'])
-n_neurons_per_layer = cfg['layers'][1]['config']['units']
-n_neurons_tot = (n_layers-1)*n_neurons_per_layer
-a = f"Le modèle est composé de {str(n_layers)} couches:\n  - La première est une couche d'entrée spécifiant la taille de la grille: ici 6x7 = 42.\n"
-a+= f"  - Les autres sont des couches denses de neurones tous reliés entre eux.\nIci, chaque couche est composée de {str(n_neurons_per_layer)} neurones.\nLe modèle est donc composé au total de {str(n_neurons_tot)} neurones !"
-print(a)
-
-for i in range(5,-1,-1):
-    print(i)
+# Wait for the sound to finish playing
+time.sleep(1)  # Adjust this to match your beep2.wav duration
